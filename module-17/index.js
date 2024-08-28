74312b002c9dfdc51322e7f80d77dbc9bffc4b33
husna_tajrin <husnajahantajrin@gmail.com>
2024-08-29 00:54:02 +0600

    practice task added

---
+++ module-17/index.js
@@ -0,0 +1,24 @@
+// task 1
+// you went to the super market to buy some oranges and apples. Calculate how much money the shopkeeper will return?
+let orange = 400;
+let apple = 300;
+let givenMoney = 1000;
+let Return = givenMoney-(orange+apple);
+console.log("shopkeeper will return :",Return);
+
+//task 2
+//write a program to calculate your average marks of bangla,english,physics, chemistry and biology marks.
+
+let bangla = 75.25;
+let english = 65;
+let physics = 80;
+let chemistry = 35.45;
+let biology = 99.50;
+let average = (bangla+english+physics+chemistry+biology)/5;
+console.log("average marks =", average);
+
+//task 3
+//your task is to divide the given number by 5 and show the reminder as the output.
+let a = 119;
+let reminder = 119%5;
+console.log("reminder is :",reminder);
